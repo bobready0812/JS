2dabf41a2b994d16701516c0212b08f182cc6e03
YeajinRyu0101 <edinryu0101@gmail.com>
2022-01-23 22:51:32 +0900

    12

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,2 +1,8 @@
 const clock = document.querySelector("h2#clock");
-clock.innerText = "lalalalal";
+
+function getClock(){
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
+getClock()
+setInterval(getClock,1000);
