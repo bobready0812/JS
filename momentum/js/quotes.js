a7a479feb287dcde9a2d8c98e65a5f43d413481c
YeajinRyu0101 <edinryu0101@gmail.com>
2022-01-24 22:31:27 +0900

    1q1

--- momentum/js/quotes.js
+++ momentum/js/quotes.js
@@ -18,4 +18,7 @@ const quotes = [
 const quote = document.querySelector("#quote span: first-child");
 const author = document.querySelector("quote span: last-child";
 
-console.log(quotes[Math.floor(Math.random() * 10))])
+const todayQuote= quotes[Math.floor(Math.random() * quotes.length))];
+
+quote.innerText = todayQuote.quoter;
+author.innerText = todayQuote.author;
