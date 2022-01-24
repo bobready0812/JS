b0105e6a00ce19659ed0f2db69e27683f62ecdcb
YeajinRyu0101 <edinryu0101@gmail.com>
2022-01-24 22:56:03 +0900

    ㅂ212

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -3,5 +3,6 @@ const pictures = [
     "cloudy.jpeg",
     "rainy.jpeg"
 ];
+const pictures = ["sunny.jpeg"."cloudy.jpeg","rainy.jpeg"];
 const chosenPictures = pictures[Math.floor(Math.random() * quotes.length)];\
-console.log(pictures);
+const bgImage = document
