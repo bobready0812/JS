f98d4853784697eb2d1405b6490c4641f5fe79cd
YeajinRyu0101 <edinryu0101@gmail.com>
2022-01-30 22:23:22 +0900

    9

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,8 +1,19 @@
+const API_KEY="0cfb02cfe48b323246fad9fc60b9f9d2";
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child")
+            const city = document.querySelector("#weather span:last-child")
+            city.innerText=data.name;
+            weather.innerText=`${data.weather[0].main}/${data.main.temp}`;
+    });
 }
+
 function onGeoError(){
     alert("Can't find you. No weather for you.")
 }
